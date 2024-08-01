27228edd4892c8a74bc7aa262453fb76a95ca5d7
Katarina Vucicevic <kajson@Katarinas-MacBook-Pro.local>
2024-08-01 13:42:22 +0200

    Initial configuration setup

---
+++ AdminService/src/main/kotlin/group/adminservice/AdminServiceApplication.kt
@@ -0,0 +1,11 @@
+package group.adminservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdminServiceApplication>(*args)
+}
